4b97a2fd2b311610f046309afea94d3473ef02c8
fujun zhang <66351840+1754699523@users.noreply.github.com>
2023-10-27 16:02:20 +0800

    Delete data directory

--- data/anlg/t.py
+++
@@ -1,6 +0,0 @@
-lines = []
-with open("test.target")as f:
-    lines = f.readlines()
-with open("1.target",'w',encoding='utf-8') as f:
-    for i in range(len(lines)):
-        f.write(str(lines[i]).split('\t'))
